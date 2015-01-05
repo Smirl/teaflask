0a5075955603e9e8ad1cf75b104ff274757b74dc
Alex Williams <alexwilliams@skylap445.corp.skyscanner.local>
2015-01-05 14:33:34 +0000

    misc - dotfiles and manage, fix requirement encoding.

--- manage.py
+++ manage.py
@@ -30,9 +30,16 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    from subprocess import call
+    args = [
+        'nosetests',
+        'app',
+        '--with-coverage',
+        '--cover-package=app',
+        '--cover-html',
+        '--cover-inclusive',
+    ]
+    call(args)
 
 
 @manager.command
